53f247df02305be4724e8b182ae38351c8694c8d
littleWhite <1430819105@qq.com>
2019-05-05 17:27:08 +0800

    错误信息抛出

--- all-service/src/main/java/com/todo/system/service/service/SysAdminService.java
+++ all-service/src/main/java/com/todo/system/service/service/SysAdminService.java
@@ -1,10 +1,11 @@
 package com.todo.system.service.service;
 
 import com.todo.system.entity.po.SysAdmin;
+import com.todo.system.util.exception.ParamNullException;
 
 public interface SysAdminService {
 
     // 用户名和密码取用户信息
-    SysAdmin getSingleAdmin(String userName, String password);
+    SysAdmin getSingleAdmin(String userName, String password) throws ParamNullException;
 
 }
