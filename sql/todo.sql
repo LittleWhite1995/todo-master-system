223aa03e1f7cf0460a657fcf66acd187d88b9dd4
littleWhite <1430819105@qq.com>
2019-06-03 18:01:53 +0800

    提交表结构

--- sql/todo.sql
+++ sql/todo.sql
@@ -1,5 +1,27 @@
+-- ----------------------------
+-- Table structure for member_info
+-- ----------------------------
+DROP TABLE IF EXISTS `member_info`;
+CREATE TABLE `member_info` (
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `sys_id` bigint(20) DEFAULT NULL COMMENT '账户id',
+  `headPortrait` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '头像',
+  `nick` varchar(55) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '昵称',
+  `signature` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '签名',
+  `sex` int(1) DEFAULT NULL COMMENT '性别,0男、1女',
+  `phone` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '手机号',
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '邮箱',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sys_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_admin`;
 CREATE TABLE `sys_admin` (
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL COMMENT 'id',
   `user_name` varchar(50) DEFAULT NULL COMMENT '用户名',
   `password` varchar(100) DEFAULT NULL COMMENT '密码',
   `enabled` int(1) DEFAULT '0' COMMENT '是否激活,1激活、0停用',
@@ -10,13 +32,18 @@ CREATE TABLE `sys_admin` (
   UNIQUE KEY `user_name` (`user_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息';
 
+-- ----------------------------
+-- Table structure for task_info
+-- ----------------------------
+DROP TABLE IF EXISTS `task_info`;
 CREATE TABLE `task_info` (
-  `id` int(11) NOT NULL COMMENT 'id',
+  `id` bigint(20) NOT NULL COMMENT 'id',
+  `member_id` bigint(20) DEFAULT NULL COMMENT '会员id',
   `title` varchar(55) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '标题',
   `follow` int(1) DEFAULT '0' COMMENT '关注：0：未关注 1：已关注',
   `instancy` int(1) DEFAULT '0' COMMENT '紧急：程度待定',
   `done_date` datetime DEFAULT NULL COMMENT '结束时间',
-  `status` int(1) DEFAULT NULL COMMENT '任务状态：0 创建 1已完成 2...待定',
+  `status` int(1) DEFAULT NULL COMMENT '任务状态：0 创建 101已完成 102撤销...待定',
   `category` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '任务类别',
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '任务描述',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
