fe0dfa4425a785793f6205f9fd67a72db7099b7b
littleWhite <1430819105@qq.com>
2019-06-10 10:46:36 +0800

    创建Id生成器模块

---
+++ snowflake-service/src/main/java/com/todo/system/snowflake/SnowflakeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.todo.system.snowflake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowflakeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowflakeServiceApplication.class, args);
+    }
+
+}
---
+++ snowflake-service/src/test/java/com/todo/system/snowflake/SnowflakeServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.todo.system.snowflake;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SnowflakeServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
